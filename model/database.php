484b308fd0c64c33474ad10534912f5ff5e7a93f
rssmith6 <89939030+rssmith6@users.noreply.github.com>
2022-03-25 23:46:04 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -4,7 +4,7 @@ $username = 'tnu86ovjrc4uj06o';
 $password = 'ig021061rrg2kc73';
 
 try{
-    $db = new PDO($hostname, $username, $password);
+    $db = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
     $error_message = 'Database Error: ';
     $error_message .= $e->getMessage();
