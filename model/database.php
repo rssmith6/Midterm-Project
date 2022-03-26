400984acb32eab776bf4984a209c8c7c45a3181a
rssmith6 <89939030+rssmith6@users.noreply.github.com>
2022-03-25 23:24:12 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,10 +1,10 @@
 <?php
-$dsn = "kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$servername = "kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = 'tnu86ovjrc4uj06o';
 $password = ' ig021061rrg2kc73';
 
 try{
-    $db = new PDO($dsn, $username, $password);
+    $db = new PDO($servername, $username, $password);
 } catch(PDOException $e) {
     $error_message = 'Database Error: ';
     $error_message .= $e->getMessage();
